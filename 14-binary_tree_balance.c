40da08cc100495d9bb42fcfcf149cee2daba0e1e
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-12-01 15:42:52 +0300

    balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,58 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measure balance factor of binary tree
+ * @tree: binary tree
+ * Return: balance factor, 0 if tree is NULL
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_l, height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
+
+	return (height_l - height_r);
+}
+
+
+/**
+ * binary_tree_height - measures height of binary tree
+ * @tree: binary tree
+ * Return: height of tree , 0 if tree == NULL
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL)
+	{
+		height_l = 1 + binary_tree_height(tree->left);
+	}
+	else
+	{
+		height_l = 0;
+	}
+
+	if (tree->right != NULL)
+	{
+		height_r = 1 + binary_tree_height(tree->right);
+	}
+	else
+	{
+		height_r = 0;
+	}
+
+	return (height_l > height_r ? height_l : height_r);
+}
